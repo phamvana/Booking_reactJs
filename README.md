3b9f394723d4ddb201d323c8d787c7bfc65c3310
phamvana <72136536+phamvana@users.noreply.github.com>
2024-02-03 22:53:42 +0700

    Node.js MongoDB Connection

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 **link**: https://docs.google.com/document/d/153YygTcRbifqVvOdrbyE1XlBh7HYd2vqP0Tls3BoWBQ/edit?usp=sharing
 
+## Installation
+
 How to run Project
 mpn i
 npm start
+
+## Node.js MongoDB Connection
+
+npm install mongodb
+npm i dotenv
+
+npm i mongoose
--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 const app = express();
+dotenv.config();
 
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB...-> Kết nối MongoDB thành công.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected! -> Không thể kết nối với MongoDB.");
+});
 app.listen(8800, () => {
-  //connect();
+  connect();
   console.log("Connected to backend...-> Kết nối với server thành công.");
 });
