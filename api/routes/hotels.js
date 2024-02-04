7aa72300ffd0678e73e759d3ce157c175bb8149f
Pham Van A <phamvana@gmail.com>
2024-02-04 09:30:17 +0700

    Node.js MongoDB CRUD Operations

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -14,7 +14,6 @@ router.post("/", async (req, res) => {
 });
 //UPDATE
 router.put("/:id", async (req, res) => {
-  const newHotel = new Hotel(req.body);
   try {
     const updateHotel = await Hotel.findByIdAndUpdate(
       req.params.id,
@@ -29,6 +28,37 @@ router.put("/:id", async (req, res) => {
   }
 });
 //DELETE
+router.delete("/:id", async (req, res) => {
+  try {
+    await Hotel.findByIdAndDelte(
+      req.params.id
+    );
+    res.status(200).json("Hotel đã được xoá! Phạm Văn Á code");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //GET
+router.get("/:id", async (req, res) => {
+  try {
+    const hotel = await Hotel.findById(
+      req.params.id
+    );
+    res.status(200).json(hotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //GET ALL
+router.get("/", async (req, res) => {
+  try {
+    const hotels = await Hotel.findById(
+      req.params.id
+    );
+    res.status(200).json(hotels);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 export default router;
